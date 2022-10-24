97193f13dc930afe756f53ec27cd74c98b7ca0a3
Hayyunulinuha123 <3103120107@student.smktelkom-pwt.sch.id>
2022-10-24 08:20:48 +0700

    CRUD Siswa

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+/*
+*URL : http://localhost:8000/api/halo
+*METHOD: GET
+*Exect:function
+*Return: JSON => {"me": "Ganteng"}
+*/
+
+//Route::get('halo', function(){
+//  return ["me" => "ganteng"];
+//});
+
+Route::get('halo', function(){
+    $data = ["me" => "cantik"];
+
+    return $data;
 });
+
+//Route::get('helocontroller',[HeloController;;class, 'index']);
+//Route::get('helocontroller',[HeloController;;class, 'index']);
+//Route::get('helocontroller',[HeloController;;class, 'index']);
+Route::resource('helocontroller', HeloController::class);
+Route::resource('siswa', SiswaController::class);
